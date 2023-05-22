5c20b60fa15ee80ba4600ba0e279eff73a2cf002
Xasticarr <sprexford@gmail.com>
2023-05-22 11:34:38 -0700

    create JS variables
    created initial JS variables, and added global variables
    Tested function to get text value from button presses.

---
+++ calc.js
@@ -0,0 +1,24 @@
+let operator = "";
+let previousValue = "";
+let currentValue = "";
+
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
+
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".operator");
+
+    let previousScreen = document.querySelector(".previous");
+    let currentScreen = document.querySelector(".current");
+
+    numbers.forEach((number) => number.addEventListener("click", function(e) {
+        handleNumber(e.target.textContent)
+    }))
+})
+
+function handleNumber(num){
+    console.log(num);
+
+}
